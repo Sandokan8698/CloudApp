e164f1df7fa019f574c590df49f52bcb0b98b097
USUARIO <USUARIO>
2017-11-11 08:54:15 -0800

    Add project files.

---
+++ CloudApp/CloudApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace CloudApp
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
